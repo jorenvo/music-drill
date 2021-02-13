417d220d88ed142696d5915eb2d3447e7f325a8f
Joren Van Onder <joren@jvo.sh>
2021-02-13 14:16:01 -0800

    Oh god oh god

--- src/music-drill.ts
+++ src/music-drill.ts
@@ -1,1 +1,20 @@
-console.log("hello world!");
+import * as Vex from "vexflow";
+
+const vf = new Vex.Flow.Factory({
+  renderer: { elementId: "music", width: 500, height: 200 },
+});
+
+const score = vf.EasyScore();
+const system = vf.System();
+
+system
+  .addStave({
+    voices: [
+      score.voice(score.notes("C#5/q, B4, A4, G#4", { stem: "up" }), undefined),
+      score.voice(score.notes("C#4/h, C#4", { stem: "down" }), undefined),
+    ],
+  })
+  .addClef("treble")
+  .addTimeSignature("4/4");
+
+vf.draw();
