258623e855dd004a8965047f184bd073a1cf6e0e
Joren Van Onder <joren@jvo.sh>
2021-02-14 18:23:16 -0800

    Add selection buttons

--- src/controller.ts
+++ src/controller.ts
@@ -1,4 +1,4 @@
-import { PitchQuiz, Quiz } from "~quiz";
+import { PitchQuiz, Quiz, RhythmQuiz } from "~quiz";
 import Vex from "vexflow";
 
 export class Controller {
@@ -12,7 +12,38 @@ export class Controller {
     this.progressEl = document.getElementById("progress")!;
     this.answerEl = document.getElementById("answer")!;
 
+    this.setupQuizSelection();
+
     this.quiz = new PitchQuiz(this);
+  }
+
+  private setupQuizSelection() {
+    const pitchId = "startPitch";
+    const rhythmId = "startRhythm";
+
+    document
+      .querySelectorAll(`#${pitchId}, #${rhythmId}`)
+      .forEach((el: Element) =>
+        el.addEventListener("click", (_ev: Event) => this.selectNewQuiz(el.id))
+      );
+  }
+
+  private buttonIdToQuiz(id: string): Quiz {
+    switch (id) {
+      case "startPitch":
+        return new PitchQuiz(this);
+        break;
+      case "startRhythm":
+        return new RhythmQuiz(this);
+        break;
+      default:
+        throw new Error(`Unknown button id ${id}`);
+    }
+  }
+
+  private selectNewQuiz(id: string) {
+    console.log(`starting ${id}`);
+    this.quiz = this.buttonIdToQuiz(id);
   }
 
   getAnswerEl() {
